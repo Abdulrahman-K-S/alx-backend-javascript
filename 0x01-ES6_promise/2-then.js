a709f3c93263f22c4d9755fd014d92f5c873c61d
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-04-24 22:55:14 +0200

    Update 2-then.js
    Fixed the output returned from .then()

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
-      status: '200',
-      body: 'Success',
+      status: 200,
+      body: 'success',
     }))
     .catch(() => Error())
     .finally(() => console.warn('Got a response from the API'));
