82ef5054096b1cd4bdb53a7015614cc4fbfc38ae
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-04-24 22:42:38 +0200

    Moved 1-main.js into mains folder

--- 0x01-ES6_promise/mains/1-main.js
+++ 0x01-ES6_promise/mains/1-main.js
@@ -1,4 +1,4 @@
-import getFullResponseFromAPI from './1-promise';
+import getFullResponseFromAPI from '../1-promise';
 
 console.log(getFullResponseFromAPI(true));
 console.log(getFullResponseFromAPI(false));
