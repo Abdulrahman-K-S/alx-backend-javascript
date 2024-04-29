794fbe4b0ee62d891a212f09f05285bc77f1f901
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-04-29 14:30:10 +0300

    Update 0-get_list_students.js
    Changed the id to what it was supposed to be.
    
    was `3` changed to `5`

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,6 @@ export default function getListStudents() {
   return ([
     { id: '1', firstname: 'Guillaume', location: 'San Francisco' },
     { id: '2', firstname: 'James', location: 'Columbia' },
-    { id: '3', firstname: 'Serena', location: 'San Francisco' },
+    { id: '5', firstname: 'Serena', location: 'San Francisco' },
   ]);
 }
