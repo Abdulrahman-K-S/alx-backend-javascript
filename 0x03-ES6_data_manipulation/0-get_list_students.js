efcbe7f4a964fd3634c349b8e7fa98fda0c7350c
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-04-29 14:44:50 +0300

    Update 0-get_list_students.js
    Had the id variable in the object a string which lead to concatenating in task 3 rather than adding.
    
    Changed id's attribute from string to int.

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   return ([
-    { id: '1', firstname: 'Guillaume', location: 'San Francisco' },
-    { id: '2', firstname: 'James', location: 'Columbia' },
-    { id: '5', firstname: 'Serena', location: 'San Francisco' },
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
   ]);
 }
