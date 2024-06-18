5620f3cc6e9a59a43ce80769e66f3e7c008433d2
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-18 16:51:51 +0300

    fix: Fix code style

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,13 +3,13 @@ const http = require('http');
 const PORT = 12345;
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello Holberton School!");
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
 
 module.exports = app;
