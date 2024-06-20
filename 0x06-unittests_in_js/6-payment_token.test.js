9e82c3884539e17c454b382d8c29e0bfacc200c1
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-20 17:21:43 +0300

    Update 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,26 +1,26 @@
 const {
     expect,
   } = require('chai');
-  const getPaymentTokenFromAPI = require('./6-payment_token');
+const getPaymentTokenFromAPI = require('./6-payment_token');
   
-  describe('getPaymentTokenFromAPI', () => {
+describe('getPaymentTokenFromAPI', () => {
     it('should return a Promise', () => {
-      const result = getPaymentTokenFromAPI();
-      expect(result).to.be.an.instanceof(Promise);
+        const result = getPaymentTokenFromAPI();
+        expect(result).to.be.an.instanceof(Promise);
     });
-  
+
     it('should resolve with a data object when true', () => {
-      getPaymentTokenFromAPI(true).then(data => {
-        expect(data).to.be.an('object');
-        expect(data).to.have.property('data');
-        done();
-      });
+        getPaymentTokenFromAPI(true).then(data => {
+            expect(data).to.be.an('object');
+            expect(data).to.have.property('data');
+            done();
+        });
     });
   
     it('should reject with doing nothing when false', () => {
-      getPaymentTokenFromAPI(false).then(data => {
-        expect(data).to.equal('');
-        done();
-      });
+        getPaymentTokenFromAPI(false).then(data => {
+            expect(data).to.equal('');
+            done();
+        });
     });
-  });
+});
