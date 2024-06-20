24ab3cca0fbfa21642dee1d97fedb848f2e01f70
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-20 17:30:01 +0300

    Feat: Add task "9. Regex integration testing"

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.get('/', (_req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  const id = req.params.id;
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
