3149169f36ae90479903764dfeda81670c2a4d5b
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-20 16:31:35 +0300

    Feat: Add task "2. Basic test using Chai assertion library"
    While using Node assert library is completely valid, a lot of developers prefer to have a behavior driven development style. This type being easier to read and therefore to maintain.
    
    Let’s install Chai with npm:
    
    - Copy the file 1-calcul.js in a new file 2-calcul_chai.js (same content, same behavior)
    - Copy the file 1-calcul.test.js in a new file 2-calcul_chai.test.js
    - Rewrite the test suite, using expect from Chai
    Tips:
    
    - Remember that test coverage is always difficult to maintain. Using an easier style for your tests will help you
    - The easier your tests are to read and understand, the more other engineers will be able to fix them when they are modifying your code
    Requirements:
    
    - You should be able to run the test suite using npm test 2-calcul_chai.test.js
    - Every test should pass without any warning
    
    Files:
    - package.json
    - 2-calcul_chai.js
    - 2-calcul_chai.test.js

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,87 @@
+const calculateNumber = require('./2-calcul_chai');
+const chai = require('chai')
+const expect = chai.expect;
+
+describe('calculateNumber', function () {
+    describe('Type === SUM', () => {
+        it('should return the numbers added correctly', () => {
+            expect.calculateNumber('SUM', 1, 3).to.equal(4);
+        });
+    
+        it('should return the numbers added after rounding up one of the inputed integers', () => {
+            expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+        });
+    
+        it('should return the numbers added after rounding up both numbers', () => {
+            expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+        });
+    
+        it('should return the numbers added after rounding up both numbers', () => {
+            expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
+        });
+    
+        it('should return the added numbres rounded down', () => {
+            expect(calculateNumber('SUM', 0.1, 0.2)).to.equal(0);
+        });
+    });
+
+    describe('Type === SUBTRACT', () => {
+        it('should return the numbers subtracted correctly in positive', () => {
+            expect(calculateNumber('SUBTRACT', 3, 1)).to.equal(2);
+        });
+
+        it('should return the numbers subtracted correctly in negative', () => {
+            expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
+        });
+    
+        it('should return the numbers subtracted after rounding up one of the inputed integers in positive', () => {
+            expect(calculateNumber('SUBTRACT', 3.7, 1)).to.equal(3);
+        });
+
+        it('should return the numbers subtracted after rounding up one of the inputed integers in negative', () => {
+            expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
+        });
+    
+        it('should return the numbers subtracted after rounding up both numbers in positive', () => {
+            expect(calculateNumber('SUBTRACT', 3.7, 1.2)).to.equal(3);
+        });
+
+        it('should return the numbers subtracted after rounding up both numbers in negative', () => {
+            expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
+        });
+    
+        it('should return the numbers subtracted after rounding up both numbers in positive', () => {
+            expect(calculateNumber('SUBTRACT', 3.7, 1.5)).to.equal(2);
+        });
+
+        it('should return the numbers subtracted after rounding up both numbers in negative', () => {
+            expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
+        });
+    
+        it('should return the subtracted numbres rounded down', () => {
+            expect(calculateNumber('SUBTRACT', 0.1, 0.2)).to.equal(0);
+        });
+    });
+
+    describe('Type === DIVIDE', () => {
+        it('should return the numbers divided after rounding up the 1st number', () => {
+            expect(calculateNumber('DIVIDE', 7.8, 4)).to.equal(2);
+        });
+
+        it ('should return the numbers divided after rounding up the 2nd number', () => {
+            expect(calculateNumber('DIVIDE', 4, 8.3)).to.equal(0.5);
+        });
+
+        it ('should return an Error after trying to divide by a 0', () => {
+            expect(calculateNumber('DIVIDE', 3.9, 0)).to.equal('Error');
+        });
+
+        it('should return an Error after trying to divide on a rounded down 0', () => {
+            expect(calculateNumber('DIVIDE', 2, 0.2)).to.be.a('string');
+        });
+
+        it('should return the numbres divided after rounding', () => {
+            expect(calculateNumber('DIVIDE', 0.3, 6.5)).to.equal(0);
+        });
+    });
+})
